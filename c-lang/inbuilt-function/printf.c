62ef82ac6093b3919d0869fe7adb19d9f145299c
EC2 Default User <ec2-user@ip-172-31-38-42.ec2.internal>
2024-09-06 17:36:35 +0000

    inbuilt

---
+++ c-lang/inbuilt-function/printf.c
@@ -0,0 +1,19 @@
+// print function for different variables
+
+#include<stdio.h>
+
+int main(){
+    int a=10;
+    int b=20;
+    int c;
+    c=a+b;
+    char d='A';
+    
+    printf("The value of a and b is :%d\n",c);
+    printf("the value of c is: %s",d);
+    printf("the address of c is:%u\n",c);
+    printf("the hex addres is:%x\n",c);
+    printf("the octa address is:%o \n",c);
+}
+
+
---
+++ c-lang/inbuilt-function/scanf.c
@@ -0,0 +1,14 @@
+// Scanf  function 
+
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("enter the a value:");
+    scanf("%d",&a);
+    printf("enter the b value:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the c value is :%d",c);
+}
+
